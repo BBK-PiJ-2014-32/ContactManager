8c910fa5237121f6034a9dbfdb7a8ffbc673b24c
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-15 19:24:26 +0000

    checkForFile code added

--- src/contactManager/FileReaderImpl.java
+++ src/contactManager/FileReaderImpl.java
@@ -1,8 +1,21 @@
 package contactManager;
 
+import java.io.*;
+
 public class FileReaderImpl implements FileReader{
 
+	
 	public boolean checkForFile(){
+		try{
+			File dataStore = new File("contactManager.csv");
+				if(dataStore.exists() == true){
+					return true;
+				} else {
+					return dataStore.createNewFile();
+				}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 		return false;
 	}
 }
