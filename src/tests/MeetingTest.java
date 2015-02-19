aebeb85f44da06633726ee94ca533713e4b2a438
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-19 14:34:59 +0000

    getIdTest added and fails

--- src/tests/MeetingTest.java
+++ src/tests/MeetingTest.java
@@ -2,13 +2,22 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import org.junit.Test;
+
+import contactManager.Meeting;
+import contactManager.MeetingImpl;
 
 public class MeetingTest {
 
 	@Test
 	public void getIdTest() {
-		
+		Meeting testMeet = new MeetingImpl();
+		int output = testMeet.getId();
+		int expected = 1;
+		assertEquals(expected, output);
 	}
 
 }
