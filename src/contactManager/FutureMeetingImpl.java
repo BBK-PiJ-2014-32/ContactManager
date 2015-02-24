d4bacefa6ddbd6683dabe176ff296210d05c1135
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-24 13:31:14 +0000

    superclass constructor added and test now passes

--- src/contactManager/FutureMeetingImpl.java
+++ src/contactManager/FutureMeetingImpl.java
@@ -1,5 +1,11 @@
 package contactManager;
 
-public class FutureMeetingImpl {
+import java.util.Set;
 
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+
+	public FutureMeetingImpl(int day, int month, int year, Set<Contact> contactSet){
+		super(day, month, year, contactSet);
+	}
+	
 }
