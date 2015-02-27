ebb02c3d1bf8a30b7f3361077f2284cc9574e0fd
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-27 09:34:17 +0000

    Javadoc shell added.

--- src/contactManager/MeetingImpl.java
+++ src/contactManager/MeetingImpl.java
@@ -3,13 +3,30 @@ package contactManager;
 import java.util.Calendar;
 import java.util.Set;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MeetingImpl.
+ */
 public class MeetingImpl implements Meeting {
 
+	/** The Id. */
 	private int Id = 0;
+	
+	/** The date. */
 	private Calendar date;
+	
+	/** The last id. */
 	private static int lastId = 0;
+	
+	/** The contact set. */
 	private Set<Contact> contactSet;
 	
+	/**
+	 * Instantiates a new meeting impl.
+	 *
+	 * @param date the date
+	 * @param contactSet the contact set
+	 */
 	public MeetingImpl(Calendar date, Set<Contact> contactSet){
 		if(contactSet == null){
 			throw new IllegalArgumentException("One or more contacts must be added");
@@ -22,16 +39,25 @@ public class MeetingImpl implements Meeting {
 		this.contactSet = contactSet;
 	}
 	
+	/* (non-Javadoc)
+	 * @see contactManager.Meeting#getId()
+	 */
 	@Override
 	public int getId() {
 		return Id;
 	}
 
+	/* (non-Javadoc)
+	 * @see contactManager.Meeting#getDate()
+	 */
 	@Override
 	public Calendar getDate() {
 		return date;
 	}
 
+	/* (non-Javadoc)
+	 * @see contactManager.Meeting#getContacts()
+	 */
 	@Override
 	public Set<Contact> getContacts() {
 		return contactSet;
