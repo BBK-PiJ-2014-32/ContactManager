4695e054d4599446dc2918cd8e4dc26c41c1f109
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-27 09:33:18 +0000

    Javadoc shell added.

--- src/contactManager/ContactManagerImpl.java
+++ src/contactManager/ContactManagerImpl.java
@@ -5,10 +5,22 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Set;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ContactManagerImpl.
+ */
 public class ContactManagerImpl implements ContactManager {
 	
+	/** The current time. */
 	private Calendar currentTime = Calendar.getInstance();
 	
+	/**
+	 * Adds the future meeting.
+	 *
+	 * @param contacts the contacts
+	 * @param date the date
+	 * @return the int
+	 */
 	@Override
 	public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
 		if(currentTime.after(date)){
@@ -18,42 +30,85 @@ public class ContactManagerImpl implements ContactManager {
 		return newFutureMeeting.getId(); 
 	}
 
+	/**
+	 * Gets the past meeting.
+	 *
+	 * @param id the id
+	 * @return the past meeting
+	 */
 	@Override
 	public PastMeeting getPastMeeting(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Gets the future meeting.
+	 *
+	 * @param id the id
+	 * @return the future meeting
+	 */
 	@Override
 	public FutureMeeting getFutureMeeting(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Gets the meeting.
+	 *
+	 * @param id the id
+	 * @return the meeting
+	 */
 	@Override
 	public Meeting getMeeting(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Gets the future meeting list.
+	 *
+	 * @param contact the contact
+	 * @return the future meeting list
+	 */
 	@Override
 	public List<Meeting> getFutureMeetingList(Contact contact) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Gets the future meeting list.
+	 *
+	 * @param date the date
+	 * @return the future meeting list
+	 */
 	@Override
 	public List<Meeting> getFutureMeetingList(Calendar date) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Gets the past meeting list.
+	 *
+	 * @param contact the contact
+	 * @return the past meeting list
+	 */
 	@Override
 	public List<PastMeeting> getPastMeetingList(Contact contact) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Adds the new past meeting.
+	 *
+	 * @param contacts the contacts
+	 * @param date the date
+	 * @param text the text
+	 */
 	@Override
 	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
 			String text) {
@@ -61,30 +116,57 @@ public class ContactManagerImpl implements ContactManager {
 		
 	}
 
+	/**
+	 * Adds the meeting notes.
+	 *
+	 * @param id the id
+	 * @param text the text
+	 */
 	@Override
 	public void addMeetingNotes(int id, String text) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Adds the new contact.
+	 *
+	 * @param name the name
+	 * @param notes the notes
+	 */
 	@Override
 	public void addNewContact(String name, String notes) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Gets the contacts.
+	 *
+	 * @param ids the ids
+	 * @return the contacts
+	 */
 	@Override
 	public Set<Contact> getContacts(int... ids) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Gets the contacts.
+	 *
+	 * @param name the name
+	 * @return the contacts
+	 */
 	@Override
 	public Set<Contact> getContacts(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Flush.
+	 */
 	@Override
 	public void flush() {
 		// TODO Auto-generated method stub
