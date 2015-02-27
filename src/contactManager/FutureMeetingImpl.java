a4e97a26ea12da893b54692132c1924b26df3978
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-27 09:33:38 +0000

    Javadoc shell added.

--- src/contactManager/FutureMeetingImpl.java
+++ src/contactManager/FutureMeetingImpl.java
@@ -3,8 +3,18 @@ package contactManager;
 import java.util.Set;
 import java.util.Calendar;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FutureMeetingImpl.
+ */
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 
+	/**
+	 * Instantiates a new future meeting impl.
+	 *
+	 * @param date the date
+	 * @param contactSet the contact set
+	 */
 	public FutureMeetingImpl(Calendar date, Set<Contact> contactSet){
 		super(date, contactSet);
 	}
