61b4f665c06056944d8768edb50dc8e411d7ae06
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-08 19:15:05 +0000

    first commit

---
+++ src/contactManager/XMLHandlerImpl.java
@@ -0,0 +1,11 @@
+package contactManager;
+
+public class XMLHandlerImpl implements XMLHandler {
+
+	@Override
+	public boolean checkForFile() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
