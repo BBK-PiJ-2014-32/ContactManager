c5b3f4ba38e3f4d50b4d7dd564c19fa874ae1472
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-08 19:12:18 +0000

    XMLHandler interface added

---
+++ src/contactManager/XMLHandler.java
@@ -0,0 +1,17 @@
+package contactManager;
+
+
+
+/**
+ * The Interface WriteToXML deals with all the writing and retrieving from the.
+ */
+public interface XMLHandler {
+	
+/**
+* Checks if a file for contacts and meeting already exists.
+*
+* @return true, if file exists.
+*/
+boolean checkForFile();	
+
+}
