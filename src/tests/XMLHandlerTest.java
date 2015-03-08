7397cb3e251266deef7e924dd7e8d12f7d1a0bf8
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-08 19:53:02 +0000

    Basic test added

--- src/tests/XMLHandlerTest.java
+++ src/tests/XMLHandlerTest.java
@@ -1,6 +1,10 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import contactManager.*;
 
 import org.junit.Test;
@@ -17,6 +21,19 @@ public class XMLHandlerTest {
 	
 	@Test
 	public void contactWriteAndLastIdGetTest(){
-		
+		XMLHandler testWTX = new XMLHandlerImpl();
+		Set<Contact> contactSet = new LinkedHashSet<Contact>();
+		Contact contact1 = new ContactImpl("Mr Man");
+		Contact contact2 = new ContactImpl("Miss Miss");
+		Contact contact3 = new ContactImpl("Mr Smith");
+		Contact contact4 = new ContactImpl("Mrs Ladyla");
+		contactSet.add(contact1);
+		contactSet.add(contact2);
+		contactSet.add(contact3);
+		contactSet.add(contact4);
+		testWTX.contactWrite(contactSet);
+		int output = testWTX.getLastContactId();
+		int expected = 4;
+		assertEquals(expected, output);
 	}
 }
