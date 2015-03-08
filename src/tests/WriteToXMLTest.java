f4a657e49870f7d7b9a9b80b00767691e8c66e5c
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-08 19:06:23 +0000

    WriteToXML started, switching to DOM

---
+++ src/tests/WriteToXMLTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import contactManager.*;
+
+import org.junit.Test;
+
+public class WriteToXMLTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
