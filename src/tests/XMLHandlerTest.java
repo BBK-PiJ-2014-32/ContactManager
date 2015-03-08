a5202c36237c218050423acca1199c0d2ffc34d7
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-08 19:10:00 +0000

    Refactored

---
+++ src/tests/XMLHandlerTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+import contactManager.*;
+
+import org.junit.Test;
+
+public class XMLHandler {
+
+	@Test
+	public void checkForFiletest() {
+		WriteToXML testWTX = new XMLHandlerImpl();
+		boolean output = testWTX.checkForFile();
+		boolean expected = true;
+		assertEquals(expected, output);
+
+	}
+}
