8a9898fcd707523fbfddc7eb23eeaa2a31bdc12c
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-08 19:19:58 +0000

    checkForFile used from old ReaderFile class.

--- src/contactManager/XMLHandlerImpl.java
+++ src/contactManager/XMLHandlerImpl.java
@@ -1,10 +1,22 @@
 package contactManager;
 
+import java.io.File;
+import java.io.IOException;
+
 public class XMLHandlerImpl implements XMLHandler {
 
 	@Override
-	public boolean checkForFile() {
-		// TODO Auto-generated method stub
+	public boolean checkForFile(){
+		try{
+			File dataStore = new File("ContactManager.xml");
+				if(dataStore.exists() == true){
+					return true;
+				} else {
+					return dataStore.createNewFile();
+				}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 		return false;
 	}
 
