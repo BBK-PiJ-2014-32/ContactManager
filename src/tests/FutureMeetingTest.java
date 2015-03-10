519ac5f4caba85300c14452774838f4dbf62a26c
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-10 22:09:38 +0000

    futureMeetingTest updated to reflect meeting constructor change. Now fails.

--- src/tests/FutureMeetingTest.java
+++ src/tests/FutureMeetingTest.java
@@ -29,15 +29,15 @@ public class FutureMeetingTest {
 	public void futureMeetingTest() {
 		Calendar date = new GregorianCalendar(2015, 4, 12);
 		Set<Contact> contactSet = new LinkedHashSet<Contact>();
-		Contact contact1 = new ContactImpl("Mr Man");
-		Contact contact2 = new ContactImpl("Miss Miss");
-		Contact contact3 = new ContactImpl("Mr Smith");
-		Contact contact4 = new ContactImpl("Mrs Ladyla");
+		Contact contact1 = new ContactImpl("Mr Man", 1);
+		Contact contact2 = new ContactImpl("Miss Miss", 2);
+		Contact contact3 = new ContactImpl("Mr Smith", 3);
+		Contact contact4 = new ContactImpl("Mrs Ladyla", 4);
 		contactSet.add(contact1);
 		contactSet.add(contact2);
 		contactSet.add(contact3);
 		contactSet.add(contact4);
-		FutureMeeting testMeet = new FutureMeetingImpl(date,contactSet);
+		FutureMeeting testMeet = new FutureMeetingImpl(date,contactSet, 1);
 		Set<Contact> outputSet = testMeet.getContacts();
 		Iterator<Contact> it = outputSet.iterator();
 		String output = "";
