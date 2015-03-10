5301785fa8fc789e603e854b048dccfe8b4fc782
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-10 22:11:42 +0000

    all tests updated to reflect changes. Now all fail.

--- src/tests/PastMeetingTest.java
+++ src/tests/PastMeetingTest.java
@@ -27,12 +27,12 @@ public class PastMeetingTest {
 	public void addPastMeetingTest(){
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
 		Set<Contact> contactSet = new LinkedHashSet<Contact>();
-		Contact contact1 = new ContactImpl("Mr Man");
-		Contact contact2 = new ContactImpl("Miss Miss");
-		Contact contact3 = new ContactImpl("Mr Smith");
-		Contact contact4 = new ContactImpl("Mrs Ladyla");
+		Contact contact1 = new ContactImpl("Mr Man", 1);
+		Contact contact2 = new ContactImpl("Miss Miss", 2);
+		Contact contact3 = new ContactImpl("Mr Smith", 3);
+		Contact contact4 = new ContactImpl("Mrs Ladyla", 4);
 		Calendar date = new GregorianCalendar(2015, 4, 12);
-		PastMeeting testPM = new PastMeetingImpl(date, contactSet, "Some Notes");
+		PastMeeting testPM = new PastMeetingImpl(date, contactSet, "Some Notes", 1);
 		Calendar outputDate = testPM.getDate();
 		String output = sdf.format(outputDate.getTime());
 		Calendar expectedCal = sdf.getCalendar();
@@ -50,8 +50,8 @@ public class PastMeetingTest {
 	public void getPastMeetingNotesTest(){
 		Calendar date = new GregorianCalendar(2015, 4, 12);
 		Set<Contact> contactSet = new LinkedHashSet<Contact>();
-		Contact contact1 = new ContactImpl("Mr Man");
-		PastMeeting testPM = new PastMeetingImpl(date, contactSet, "Some Notes");
+		Contact contact1 = new ContactImpl("Mr Man", 1);
+		PastMeeting testPM = new PastMeetingImpl(date, contactSet, "Some Notes", 1);
 		String output = testPM.getNotes();
 		String expected = "Some Notes";
 		assertEquals(expected, output);
