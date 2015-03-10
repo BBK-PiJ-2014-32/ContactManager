7d8f9720d688f41dc3b8156e2a347154598999cb
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-10 14:13:19 +0000

    Dead end with JAXB going back to ContactManagerImpl

--- src/contactManager/AdaptedContact.java
+++ src/contactManager/AdaptedContact.java
@@ -1,5 +1,6 @@
 package contactManager;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
--- src/contactManager/XMLHandlerImpl.java
+++ src/contactManager/XMLHandlerImpl.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.LinkedHashSet;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 
 
@@ -41,8 +43,10 @@ public class XMLHandlerImpl implements XMLHandler {
 			
 			AdaptedContact adapCon = new AdaptedContact();
 			Iterator<Contact> it = contactSet.iterator();
-			adapCon.setCon(it.next());
-			jaxbMarshaller.marshal(adapCon, dataStore);
+			while(it.hasNext()){
+				adapCon.setCon(it.next());
+				jaxbMarshaller.marshal(adapCon, dataStore);
+			}
 			
 		} catch (JAXBException ex){
 			ex.printStackTrace();
@@ -53,8 +57,26 @@ public class XMLHandlerImpl implements XMLHandler {
 
 	@Override
 	public int getLastContactId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+		try{
+			File file = new File("ContactManager.xml");
+			JAXBContext context = JAXBContext.newInstance(AdaptedContact.class);
+			Unmarshaller um = context.createUnmarshaller();
+			AdaptedContact newCon = (AdaptedContact) um.unmarshal(file);
+			Contact contact = newCon.getContact();
+		    Set<Contact> conSet = new LinkedHashSet<Contact>();
+			conSet.add(contact);
+		    int lastId = 0;
+		    Iterator<Contact> it = conSet.iterator();
+		    
+		    while(it.hasNext()){
+		    	Contact next = it.next();
+		    	lastId = next.getId();
+		    	return lastId;
+		    	}	
+		    return lastId; 
+		} catch (JAXBException ex){
+			ex.printStackTrace();
+		}
+	return 0;
+	}	
 }
