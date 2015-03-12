bad334868f83aa0452bfa3408fc5cba7b0aaadf7
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-12 15:03:33 +0000

    Basic test code added.

--- src/tests/ContactManagerTest.java
+++ src/tests/ContactManagerTest.java
@@ -183,6 +183,33 @@ public class ContactManagerTest {
 		
 		@Test
 		public void getFutureMeetingListByContactTest(){
+			ContactManager testCM = new ContactManagerImpl();
+			Set<Contact> contactSet = new LinkedHashSet<Contact>();
+			Contact contact1 = new ContactImpl("Mr Man", 1);
+			Contact contact2 = new ContactImpl("Some Man", 2);
+			Contact contact3 = new ContactImpl("A Woman", 3);
+			Contact contact4 = new ContactImpl("Anon", 4);
+			contactSet.add(contact1);
+			contactSet.add(contact2);
+			contactSet.add(contact3);
+			contactSet.add(contact4);
+			Set<Contact> contactSet2 = new LinkedHashSet<Contact>();
+			Contact contact5 = new ContactImpl("Andrew", 5);
+			contactSet2.add(contact5);
+			testCM.addFutureMeeting(contactSet, new GregorianCalendar(2015, 3, 15));
+			testCM.addFutureMeeting(contactSet, new GregorianCalendar(2015, 4, 12));
+			testCM.addFutureMeeting(contactSet, new GregorianCalendar(2015, 07, 30));
+			testCM.addFutureMeeting(contactSet2, new GregorianCalendar(2016, 1, 1));
+			testCM.addFutureMeeting(contactSet, new GregorianCalendar(2015, 06, 30));
+			List<Meeting> outList = testCM.getFutureMeetingList(contact4);
+			int [] expectId = {1, 2, 3, 5};
+			Iterator<Meeting> it = outList.iterator();
+			int i = 0;
+			while(it.hasNext()){
+				int outId = it.next().getId();
+				assertEquals(expectId[i], outId);
+				i++;
+				}
 			
 		}
 }
