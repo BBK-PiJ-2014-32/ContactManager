1fa1b9161c0b2bd03f51cd6ea085ceb6f40c7403
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-15 21:32:24 +0000

    FileObjects interface added

---
+++ src/contactManager/FileObjects.java
@@ -0,0 +1,15 @@
+package contactManager;
+
+/**
+ * Wrapper class to hold Meeting and Contact objects before being written to file.
+ */
+public interface FileObjects {
+	
+
+/**
+ * Gets the object.
+ *
+ * @return the object
+ */
+Object getObject();
+}
