657b28d24a802d78d9ce6d38fd8a00458b473010
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-17 11:52:01 +0000

    secondFlushTest added.

--- src/tests/ContactManagerTest.java
+++ src/tests/ContactManagerTest.java
@@ -291,7 +291,28 @@ public class ContactManagerTest {
 				assertEquals(expected[i], output.getName());
 				i++;
 			}
-			
+		}
+		
+		@Test
+		public void secondFlushTest(){
+			ContactManager testCM1 = new ContactManagerImpl();
+			testCM1.addNewContact("Mr Man", "He's the man");
+			testCM1.addNewContact("Miss SomeOne", "She's not a man");
+			testCM1.addNewContact("Mr Testy", "Testing");
+			testCM1.addFutureMeeting(testCM1.getContacts(1,2,3), new GregorianCalendar(2015, 8, 15));
+			testCM1.flush();
+			ContactManager testCM2 = new ContactManagerImpl();
+			Meeting testMeet = testCM2.getFutureMeeting(1);
+			Set<Contact> outSet = new LinkedHashSet<Contact>();
+			outSet = testMeet.getContacts();
+			int i = 0;
+			String [] expected = {"Mr Man", "Miss SomeOne", "Mr Testy"};
+			Iterator<Contact> it = outSet.iterator();
+			while(it.hasNext()){
+				Contact output = it.next();
+				assertEquals(expected[i], output.getName());
+				i++;
+			}
 		}
 		
 }
