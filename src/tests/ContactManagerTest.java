8fc42f14ea806d8f97a9892e129ae69e99595622
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-21 19:58:58 +0000

    cleaned up

--- src/tests/ContactManagerTest.java
+++ src/tests/ContactManagerTest.java
@@ -271,50 +271,6 @@ public class ContactManagerTest {
 			testCM.addFutureMeeting(testCM.getContacts(1,2,3), new GregorianCalendar(2015, 8, 15));
 			testCM.getPastMeeting(1);
 		}
-	
-		/*@Test
-		public void zfirstFlushTest(){
-			ContactManager testCM1 = new ContactManagerImpl();
-			testCM1.addNewContact("Mr Man", "He's the man");
-			testCM1.addNewContact("Miss SomeOne", "She's not a man");
-			testCM1.addNewContact("Mr Testy", "Testing");
-			testCM1.flush();
-			ContactManager testCM2 = new ContactManagerImpl();
-			Set<Contact> outSet = new LinkedHashSet<Contact>(); 
-			outSet = testCM2.getContacts(1, 2, 3);
-			int i = 0;
-			String [] expected = {"Mr Man", "Miss SomeOne", "Mr Testy"};
-			Iterator<Contact> it = outSet.iterator();
-			System.out.println(outSet.size());
-			while(it.hasNext()){
-				Contact output = it.next();
-				assertEquals(expected[i], output.getName());
-				i++;
-			}
-		}
-		
-		@Test
-		public void zsecondFlushTest(){
-			ContactManager testCM1 = new ContactManagerImpl();
-			testCM1.addNewContact("Mr Man", "He's the man");
-			testCM1.addNewContact("Miss SomeOne", "She's not a man");
-			testCM1.addNewContact("Mr Testy", "Testing");
-			testCM1.addFutureMeeting(testCM1.getContacts(1,2,3), new GregorianCalendar(2015, 8, 15));
-			testCM1.flush();
-			ContactManager testCM2 = new ContactManagerImpl();
-			Meeting testMeet = testCM2.getFutureMeeting(1);
-			System.out.println(testMeet);
-			Set<Contact> outSet = new LinkedHashSet<Contact>();
-			outSet = testMeet.getContacts();
-			int i = 0;
-			String [] expected = {"Mr Man", "Miss SomeOne", "Mr Testy"};
-			Iterator<Contact> it = outSet.iterator();
-			while(it.hasNext()){
-				Contact output = it.next();
-				assertEquals(expected[i], output.getName());
-				i++;
-			}
-		}*/
 		
 }
 
