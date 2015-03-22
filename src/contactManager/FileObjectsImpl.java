814471dff2105d7b4ffbf19423968c0e4437e00b
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-22 20:53:17 +0000

    Cleaned up and ready for submission.

--- src/contactManager/FileObjectsImpl.java
+++ src/contactManager/FileObjectsImpl.java
@@ -1,13 +1,26 @@
 package contactManager;
 
+/**
+ * The Class FileObjectsImpl which implements the FileObjects interface
+ * @see contactManager.FileObjects
+ */
 public class FileObjectsImpl implements FileObjects{
 
+	/** The object. */
 	private Object theObject;
 	
+	/**
+	 * Instantiates a new file objects.
+	 *
+	 * @param anObject the object to wrapped.
+	 */
 	public FileObjectsImpl(Object anObject){
 		theObject = anObject;
 	}
 	
+	/** 
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Object getObject() {
 		return theObject;
