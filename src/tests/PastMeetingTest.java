3aea28c13ce22da7903f20e260e2579d12b00e47
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-22 21:11:33 +0000

    PastMeetingTest cleaned up and ready for submission.

--- src/tests/PastMeetingTest.java
+++ src/tests/PastMeetingTest.java
@@ -6,22 +6,19 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Date;
-import java.time.*;
 import java.text.*;
 
 import org.junit.Test;
 
 import contactManager.*;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class PastMeetingTest.
  */
 public class PastMeetingTest {
 
 	/**
-	 * Adds the past meeting test.
+	 * Tests that the PastMeetingImpl class is working correctly.
 	 */
 	@Test
 	public void addPastMeetingTest(){
@@ -31,6 +28,10 @@ public class PastMeetingTest {
 		Contact contact2 = new ContactImpl("Miss Miss", 2);
 		Contact contact3 = new ContactImpl("Mr Smith", 3);
 		Contact contact4 = new ContactImpl("Mrs Ladyla", 4);
+		contactSet.add(contact1);
+		contactSet.add(contact2);
+		contactSet.add(contact3);
+		contactSet.add(contact4);
 		Calendar date = new GregorianCalendar(2015, 4, 12);
 		PastMeeting testPM = new PastMeetingImpl(date, contactSet, "Some Notes", 1);
 		Calendar outputDate = testPM.getDate();
@@ -42,15 +43,14 @@ public class PastMeetingTest {
 	}
 
 	/**
-	 * Gets the past meeting notes test.
-	 *
-	 * @return the past meeting notes test
+	 * Tests that the getNotes method in PastMeetingImpl is working correctly.
 	 */
 	@Test
 	public void getPastMeetingNotesTest(){
 		Calendar date = new GregorianCalendar(2015, 4, 12);
 		Set<Contact> contactSet = new LinkedHashSet<Contact>();
 		Contact contact1 = new ContactImpl("Mr Man", 1);
+		contactSet.add(contact1);
 		PastMeeting testPM = new PastMeetingImpl(date, contactSet, "Some Notes", 1);
 		String output = testPM.getNotes();
 		String expected = "Some Notes";
