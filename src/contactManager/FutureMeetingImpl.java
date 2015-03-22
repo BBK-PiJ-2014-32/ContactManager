93049c863686cd14c65fde57cdf39daa239cbec7
Phil Hannant <philip.a.hannant@gmail.com>
2015-03-22 20:36:09 +0000

    FutureMeetingImpl cleaned up and ready for submission.

--- src/contactManager/FutureMeetingImpl.java
+++ src/contactManager/FutureMeetingImpl.java
@@ -3,17 +3,17 @@ package contactManager;
 import java.util.Set;
 import java.util.Calendar;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class FutureMeetingImpl.
+ * The Class FutureMeetingImpl extends the MeetingImpl class and implements the FutureMeeting interface.
  */
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 
 	/**
-	 * Instantiates a new future meeting impl.
+	 * Instantiates a new future meeting.
 	 *
-	 * @param date the date
-	 * @param contactSet the contact set
+	 * @param date the date of the meeting
+	 * @param contactSet the set of contacts to be added to the meeting
+	 * @param id the id of the meeting
 	 */
 	public FutureMeetingImpl(Calendar date, Set<Contact> contactSet, int id){
 		super(date, contactSet, id);
